103f8b1e8b9ec37f72e8e46f4e156cd703ebe819
mostfa-charfedin <akkezmostfacharfedin@gmail.com>
2025-11-26 19:08:37 +0100

    ajout des fichers pour le test

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,13 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Activate test profile
 class StudentManagementApplicationTests {
-
     @Test
     void contextLoads() {
+        // Default smoke test
     }
-
 }
